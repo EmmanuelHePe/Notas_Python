66c4e9372f5d6ea634a62ec798dd3ac353f636d5
Emmanuel HP <emmanuel.hpee96@gmail.com>
2019-03-05 22:56:14 -0600

    Initial Commit

--- python Files/Trick.py
+++ python Files/Trick.py
@@ -4,15 +4,18 @@
 
     # working directory -----(git add)>>>> staging area -------(git commi t)>>>>> repository
 Pasos
-    1.- Git Init Siempre debes iniciar
-    2.- Git add
-    3.- Git Commit
+    1.- Git Init Siempre debes iniciar ("Verde")
+    2.- Git add     (Naranja)
+    3.- Git Commit  (Sin Color)
     4.-
 
 Cosas utiles
 - Status:       Observar status de los archivos
 - Log:          Observar las versiones
-- Checkout:     dsfdsf
+- Checkout:     Distintas versiones
+- Diff:         Ver las diferencias
+- Branch:       Se crean nuevas versiones
+
 ############Comandos utiles############
 # crtl + shift + /          Comentar lineas seleccionads
 # ctrl + shift + p          Acceder a funciones Atom
